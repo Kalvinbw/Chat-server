45170a04d8b5cb8b90491ea8211ccd6ce6b5255a
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-06-16 17:35:08 -0600

    Added id for user

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('a user connected: ' + socket.id);
     socket.on('chat message', (msg) => {
         console.log('message: ' + msg);
     });
-    
+
     socket.on('disconnect', () => {
-      console.log('user disconnected');
+      console.log('user disconnected: ' + socket.id);
     });
 
     socket.on('chat message', (msg) => {
