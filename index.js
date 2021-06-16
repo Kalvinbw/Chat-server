68a58ec7e28ca47bb6b34080bd603723c03ea40f
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-06-16 17:57:47 -0600

    Update

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ io.on('connection', (socket) => {
 
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg, socket.id);
-      });
-  });
+    });
+});
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
