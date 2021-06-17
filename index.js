94a5054f74e311aefbf95840dc586579ec524389
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-06-17 16:39:10 -0600

    Users can now add a nickname

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+class User {
+  constructor(id) {
+    this.id = id;
+  }
+  name;
+  addName(n) {
+    this.name = n;
+  }
+}
+
 let users = [];
 
 app.get('/', (req, res) => {
@@ -12,17 +22,39 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    users.push(socket.id);
+    users.push(new User(socket.id));
 
     io.emit('update users', users);
 
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg, socket.id);
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].id == socket.id) {
+          if (users[i].name) {
+            io.emit('chat message', msg, users[i].name);
+            break;
+          } else {
+            io.emit('chat message', msg, socket.id);
+          break;
+          }  
+        }
+      }
+      
     });
 
+    socket.on('added name', (name) => {
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].id == socket.id) {
+          users[i].addName(name);
+          break;
+        }
+      }
+      io.emit('update users', users);
+    })
+
     socket.on('disconnect', () => {
-      users = users.filter(user => user !== socket.id);
-      console.log(users);
+      users = users.filter(user => user.id !== socket.id);
+      //delete users[socket.id];
+      console.log('Someone disconnected ' + socket.id);
       io.emit('update users', users);
     });
 
